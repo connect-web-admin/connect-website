7cef2693bb4409fc29add6ee8baeb3b7d38c1755
OBINO Takuya <espinarosa.obino@gmail.com>
2025-02-27 10:09:59 +0900

    RegisterScoreModal新規作成

---
+++ src/components/RegisterScoreModal.vue
@@ -0,0 +1,97 @@
+<script setup>
+const props = defineProps({
+    show: Boolean,
+    championshipId: String,
+    matchId: String,
+    gameStatus: String,
+    isHome: Boolean,
+    isAway: Boolean,
+    isPlusScore: Boolean,
+    isMinusScore: Boolean,
+    homeClubFirstHalfScore: Number,
+    homeClubSecondHalfScore: Number,
+    awayClubFirstHalfScore: Number,
+    awayClubSecondHalfScore: Number,
+});
+
+// CSS
+const btnBase = 'w-1/4 py-1 text-white rounded-md';
+</script>
+
+<template>
+    <Transition name="modal">
+        <div v-if="show" class="modal-mask">
+            <div class="modal-container">
+                <div class="modal-body">
+                    <slot name="body">default body</slot>
+                </div>
+
+                <div class="modal-footer">
+                    <slot name="footer">
+                        <div v-if="isPlusScore" class="flex justify-around">
+                            <button type="submit" @click="$emit('close')" :class="btnBase" class="bg-gray-400">いいえ</button>
+                            <button type="submit" v-if="isHome" @click="$emit('plusScore'), $emit('close')" :class="btnBase" class="bg-blue-500">はい</button>                            
+                            <button type="submit" v-if="isAway" @click="$emit('plusScore'), $emit('close')" :class="btnBase" class="bg-blue-500">はい</button>                            
+                        </div>
+                        <div v-if="isMinusScore" class="flex justify-around">
+                            <button type="submit" @click="$emit('close')" :class="btnBase" class="bg-gray-400">いいえ</button>
+                            <button type="submit" v-if="isHome" @click="$emit('minusScore'), $emit('close')" :class="btnBase" class="bg-blue-500">はい</button>                            
+                            <button type="submit" v-if="isAway" @click="$emit('minusScore'), $emit('close')" :class="btnBase" class="bg-blue-500">はい</button>                            
+                        </div>
+                    </slot>
+                </div>
+            </div>
+        </div>
+    </Transition>
+</template>
+
+<style>
+.modal-mask {
+    position: fixed;
+    z-index: 9998;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.2);
+    display: flex;
+    transition: opacity 0.3s ease;
+}
+
+.modal-container {
+    width: 350px;
+    margin: auto;
+    padding: 20px 30px;
+    background-color: #fff;
+    border-radius: 10px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+    transition: all 0.3s ease;
+}
+
+.modal-body {
+    margin: 20px 0;
+}
+
+/*
+ * The following styles are auto-applied to elements with
+ * transition="modal" when their visibility is toggled
+ * by Vue.js.
+ *
+ * You can easily play with the modal transition by editing
+ * these styles.
+ */
+
+.modal-enter-from {
+    opacity: 0;
+}
+
+.modal-leave-to {
+    opacity: 0;
+}
+
+.modal-enter-from .modal-container,
+.modal-leave-to .modal-container {
+    -webkit-transform: scale(1.1);
+    transform: scale(1.1);
+}
+</style>
