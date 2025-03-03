50379258ff1df7b5dab94d431dbeef2eedfac2be
OBINO Takuya <espinarosa.obino@gmail.com>
2025-03-03 17:16:53 +0900

    画像をSVGに変更、CSS修正

--- src/views/TopView.vue
+++ src/views/TopView.vue
@@ -5,12 +5,12 @@ const sampleRoundName = 'Aブロック';
 </script>
 
 <template>
-<div class="bg-white p-4">
+<div class="p-4">
     <div>
         <h1 class="text-xl">今週の試合</h1>
         <div class="bg-[#090A0A] text-[#FAFAFC] w-fit px-1">2025.2.22(Sat)</div>
         <div class="flex justify-between items-center border-t-1 border-gray-300 py-2">
-            <img src="@/assets/top-sample/keithston.png" alt="発寒南FC" class="block w-1/7 h-full">
+            <img src="@/assets/top-sample/keithston.svg" alt="発寒南FC" class="w-1/7 h-full">
             <div class="text-center w-5/7 px-2">
                 <div>
                     <p class="text-[#666C6C]">{{ sampleChampionshipName }}</p>
@@ -18,19 +18,19 @@ const sampleRoundName = 'Aブロック';
                     <p class="text-[#666C6C]">{{ sampleRoundName }}第一試合</p>
                 </div>
                 <div class="flex justify-between">
-                    <div class="block w-2/5 text-[#666C6C]">発寒南FC</div>
-                    <div class="block w-1/5 font-bold text-xl text-[#231815]">2 - 2</div>
-                    <div class="block w-2/5 text-[#666C6C]">清田FC</div>
+                    <div class="w-2/5 text-[#666C6C]">発寒南FC</div>
+                    <div class="w-1/5 text-xl text-[#231815]">2 - 2</div>
+                    <div class="w-2/5 text-[#666C6C]">清田FC</div>
                 </div>
                 <div>
                     <span class="text-[#F24D11] font-bold">2 PK 3</span>
                 </div>
             </div>
-            <img src="@/assets/top-sample/salfordBlueYellow.png" alt="清田FC" class="block w-1/7 h-full">
+            <img src="@/assets/top-sample/salfordBlueYellow.svg" alt="清田FC" class="w-1/7 h-full">
         </div>
 
         <div class="flex justify-between items-center border-t-1 border-gray-300 py-2">
-            <img src="@/assets/top-sample/salfordBlueRed.png" alt="苗穂FC" class="block w-1/7 h-full">
+            <img src="@/assets/top-sample/bigball.svg" alt="苗穂FC" class="w-1/7 h-full">
             <div class="text-center w-5/7 px-2">
                 <div>
                     <p class="text-[#666C6C]">{{ sampleChampionshipName }}</p>
@@ -38,18 +38,18 @@ const sampleRoundName = 'Aブロック';
                     <p class="text-[#666C6C]">{{ sampleRoundName }}第二試合</p>
                 </div>
                 <div class="flex justify-between">
-                    <div class="block w-2/5 text-[#666C6C]">発寒南FC</div>
-                    <div class="block w-1/5 font-bold text-xl text-[#231815]">4 - 0</div>
-                    <div class="block w-2/5 text-[#666C6C]">清田FC</div>
+                    <div class="w-2/5 text-[#666C6C]">発寒南FC</div>
+                    <div class="w-1/5 text-xl text-[#231815]">4 - 0</div>
+                    <div class="w-2/5 text-[#666C6C]">清田FC</div>
                 </div>
                 <div>
                 </div>
             </div>
-            <img src="@/assets/top-sample/borcelle.png" alt="厚別西FC" class="block w-1/7 h-full">
+            <img src="@/assets/top-sample/ball.svg" alt="厚別西FC" class="w-1/7 h-full">
         </div>
 
         <div class="flex justify-between items-center border-t-1 border-gray-300 py-2">
-            <img src="@/assets/top-sample/rimberio.png" alt="苗穂FC" class="block w-1/7 h-full">
+            <img src="@/assets/top-sample/bigball.svg" alt="苗穂FC" class="w-1/7 h-full">
             <div class="text-center w-5/7 px-2">
                 <div>
                     <p class="text-[#666C6C]">{{ sampleChampionshipName }}</p>
@@ -57,19 +57,19 @@ const sampleRoundName = 'Aブロック';
                     <p class="text-[#666C6C]">{{ sampleRoundName }}第三試合</p>
                 </div>
                 <div class="flex justify-between">
-                    <div class="block w-2/5 text-[#666C6C]">発寒南FC</div>
-                    <div class="block w-1/5 font-bold text-xl text-[#231815]">1 - 2</div>
-                    <div class="block w-2/5 text-[#666C6C]">清田FC</div>
+                    <div class="w-2/5 text-[#666C6C]">発寒南FC</div>
+                    <div class="w-1/5 text-xl text-[#231815]">1 - 2</div>
+                    <div class="w-2/5 text-[#666C6C]">清田FC</div>
                 </div>
             </div>
-            <img src="@/assets/top-sample/salfordGreenWhite.png" alt="厚別西FC" class="block w-1/7 h-full">
+            <img src="@/assets/top-sample/salfordGreenWhite.svg" alt="厚別西FC" class="w-1/7 h-full">
         </div>
     </div>
 
     <div class="mt-4">
         <div class="bg-[#090A0A] text-[#FAFAFC] w-fit px-1">2025.2.23(Sun)</div>
         <div class="flex justify-between items-center border-t-1 border-gray-300 py-2">
-            <img src="@/assets/top-sample/salfordCrown.png" alt="新琴似FC" class="block w-1/7 h-full">
+            <img src="@/assets/top-sample/salfordCrown.svg" alt="新琴似FC" class="w-1/7 h-full">
             <div class="text-center w-5/7 px-2">
                 <div>
                     <p class="text-[#666C6C]">{{ sampleChampionshipName }}</p>
@@ -77,18 +77,18 @@ const sampleRoundName = 'Aブロック';
                     <p class="text-[#666C6C]">Bブロック第一試合</p>
                 </div>
                 <div class="flex justify-between">
-                    <div class="block w-2/5 text-[#666C6C]">発寒南FC</div>
-                    <div class="block w-1/5 font-bold text-xl text-[#231815]">2 - 11</div>
-                    <div class="block w-2/5 text-[#666C6C]">清田FC</div>
+                    <div class="w-2/5 text-[#666C6C]">発寒南FC</div>
+                    <div class="w-1/5 text-xl text-[#231815]">2 - 11</div>
+                    <div class="w-2/5 text-[#666C6C]">清田FC</div>
                 </div>
                 <div>
                 </div>
             </div>
-            <img src="@/assets/top-sample/hanover.png" alt="白石FC" class="block w-1/7 h-full">
+            <img src="@/assets/top-sample/black.svg" alt="白石FC" class="w-1/7 h-full">
         </div>
 
         <div class="flex justify-between items-center border-t-1 border-gray-300 py-2">
-            <img src="@/assets/top-sample/keithston.png" alt="発寒南FC" class="block w-1/7 h-full">
+            <img src="@/assets/top-sample/keithston.svg" alt="発寒南FC" class="w-1/7 h-full">
             <div class="text-center w-5/7 px-2">
                 <div>
                     <p class="text-[#666C6C]">{{ sampleChampionshipName }}</p>
@@ -96,15 +96,15 @@ const sampleRoundName = 'Aブロック';
                     <p class="text-[#666C6C]">Bブロック第二試合</p>
                 </div>
                 <div class="flex justify-between">
-                    <div class="block w-2/5 text-[#666C6C]">発寒南FC</div>
-                    <div class="block w-1/5 font-bold text-xl text-[#231815]">0 - 0</div>
-                    <div class="block w-2/5 text-[#666C6C]">清田FC</div>
+                    <div class="w-2/5 text-[#666C6C]">発寒南FC</div>
+                    <div class="w-1/5 text-xl text-[#231815]">0 - 0</div>
+                    <div class="w-2/5 text-[#666C6C]">清田FC</div>
                 </div>
                 <div>
                     <span class="text-[#F24D11] font-bold">2 PK 3</span>
                 </div>
             </div>
-            <img src="@/assets/top-sample/salfordBlueYellow.png" alt="清田FC" class="block w-1/7 h-full">
+            <img src="@/assets/top-sample/black.svg" alt="清田FC" class="w-1/7 h-full">
         </div>
     </div>
 </div>
