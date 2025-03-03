2e324b4bf28f6ef0076d220d42a52bfa4a3568f4
OBINO Takuya <espinarosa.obino@gmail.com>
2025-03-03 14:04:55 +0900

    リンクを追加

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,118 +1,14 @@
 <script setup>
-
 </script>
 
 <template>
-<div class="footer-wrapper">
-    <div class="live-report-icon-container">
-        <img src="../assets/icons/icon_live_report.png" />
-    </div>
-    <div class="menu-wrapper">
-        <div class="menu-container">
-            <div class="menus-on-left">
-                <div class="each-icon-container">
-                    <img src="../assets/icons/icon_home.png" />
-                    <p class="icon-name">ホーム</p>
-                </div>
-                <div class="each-icon-container">
-                    <router-link to='/topics'>
-                        <img src="../assets/icons/icon_campaigne.png" />
-                        <p class="icon-name">お知らせ</p>
-                    </router-link>
-                </div>
-            </div>
-            <div class="menus-on-right">
-            <div class="each-icon-container">
-                    <img src="../assets/icons/icon_mail.png" />
-                    <p class="icon-name">コンタクト</p>
-                </div>
-                <div class="each-icon-container">
-                    <img src="../assets/icons/icon_premium.png" />
-                    <p class="premium-gradation">プレミアム</p>
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="flex justify-between items-end bg-white py-4 px-6 h-20">
+    <router-link to="/"><img src="@/assets/icons/home.png" alt="ホーム"></router-link>
+    <router-link to="/championship-schedule"><img src="@/assets/icons/championship_schedule.png" alt="大会日程"></router-link>
+    <router-link to="/live-report-for-user"><img src="@/assets/icons/live_report.png" alt="試合速報"></router-link>
+    <router-link to="/contact"><img src="@/assets/icons/contact.png" alt="コンタクト"></router-link>
+    <router-link to="/premium"><img src="@/assets/icons/premium.png" alt="プレミアム"></router-link>
 </div>
 </template>
 
-<style scoped>
-@media screen and (max-width: 500px) {
-    .footer-wrapper {
-        height: 89px;
-        width: 100%;
-        background: linear-gradient(#1F9ACB, #7FDBFF); 
-        border-top: 2px solid #FDFEFF;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        position: fixed;
-        bottom: 0;
-    }
-
-    .menu-wrapper {
-        width: 100%;
-        height: auto;
-    }
-
-    .menu-container {
-        height: 100%;
-        width: 100%;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        align-items: flex-end;
-    }
-
-    .menus-on-left, .menus-on-right {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-        width: 35%;
-    }
-
-    .menus-on-left {
-        padding-left: 10px;
-    }
-
-    .menus-on-right {
-        padding-right: 10px;
-    }
-
-    .each-icon-container {
-        text-align: center;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        align-items: center;
-    }
-
-    .each-icon-container img {
-        width: 30px;
-        height: auto;
-    }
-
-    /* コンタクトの手紙アイコンだけ浮いて見えるので余白を増やして調整 */
-    .menus-on-right .each-icon-container:first-child {
-        padding-top: 4px;
-    }
-
-    .icon-name, .premium-gradation {
-        font-size: 10px;
-        color: #FDFEFF;
-        margin-top: 2px;
-    }
-
-    .live-report-icon-container {
-        position: absolute;
-        bottom: 10px;
-    }
-    
-    .premium-gradation {
-        background: linear-gradient(90deg, #FCF2AE, #FFFEF1, #D7D542);
-        background-clip: text;
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;
-    } 
-}
-</style>
+<style></style>
