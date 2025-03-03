4653fc3853f60606779fb831028da5d8a020d4a4
OBINO Takuya <espinarosa.obino@gmail.com>
2025-03-03 17:14:19 +0900

    フッターアイコンをSVGに変更

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -2,12 +2,12 @@
 </script>
 
 <template>
-<div class="flex justify-between items-end bg-white py-4 px-6 h-20">
-    <router-link to="/"><img src="@/assets/icons/home.png" alt="ホーム"></router-link>
-    <router-link to="/championship-schedule"><img src="@/assets/icons/championship_schedule.png" alt="大会日程"></router-link>
-    <router-link to="/live-report-for-user"><img src="@/assets/icons/live_report.png" alt="試合速報"></router-link>
-    <router-link to="/contact"><img src="@/assets/icons/contact.png" alt="コンタクト"></router-link>
-    <router-link to="/premium"><img src="@/assets/icons/premium.png" alt="プレミアム"></router-link>
+<div class="flex justify-between items-end py-4 px-6 h-20">
+    <router-link to="/"><img src="@/assets/icons/home.svg" alt="ホーム"></router-link>
+    <router-link to="/championship-schedule"><img src="@/assets/icons/championship-schedule.svg" alt="大会日程"></router-link>
+    <router-link to="/live-report-for-user"><img src="@/assets/icons/live-report.svg" alt="試合速報"></router-link>
+    <router-link to="/contact"><img src="@/assets/icons/contact.svg" alt="コンタクト"></router-link>
+    <router-link to="/premium"><img src="@/assets/icons/premium.svg" alt="プレミアム"></router-link>
 </div>
 </template>
 
