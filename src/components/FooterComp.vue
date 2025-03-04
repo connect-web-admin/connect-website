fbe5e64c2714e659adc826a76b911ee6a238265b
OBINO Takuya <espinarosa.obino@gmail.com>
2025-03-04 09:36:33 +0900

    border top追加

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-<div class="flex justify-between items-end py-4 px-6 h-20">
+<div class="flex justify-between items-end py-4 px-6 h-20 border-t-1 text-gray-200">
     <router-link to="/"><img src="@/assets/icons/home.svg" alt="ホーム"></router-link>
     <router-link to="/championship-schedule"><img src="@/assets/icons/championship-schedule.svg" alt="大会日程"></router-link>
     <router-link to="/live-report-for-user"><img src="@/assets/icons/live-report.svg" alt="試合速報"></router-link>
