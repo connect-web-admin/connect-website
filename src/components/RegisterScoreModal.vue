36abc45c735af11207aa0d307097ac873089fc3c
OBINO Takuya <espinarosa.obino@gmail.com>
2025-03-07 15:32:45 +0900

    CSSをTailwindCSSに変更

--- src/components/RegisterScoreModal.vue
+++ src/components/RegisterScoreModal.vue
@@ -16,15 +16,15 @@ const btnBase = 'w-1/4 py-1 text-white rounded-md';
 
 <template>
     <Transition name="modal">
-        <div v-if="show" class="modal-mask">
-            <div class="modal-container">
-                <div class="modal-body">
+        <div v-if="show" class="fixed inset-0 z-[9998] flex bg-black/20 transition-opacity duration-300">
+            <div class="m-auto w-[350px] rounded-lg bg-white p-[20px_30px] shadow-[0_2px_8px_rgba(0,0,0,0.33)] transition-all duration-300">
+                <div class="my-5">
                     <slot name="body">default body</slot>
                 </div>
 
                 <div class="modal-footer">
                     <slot name="footer">
-                        <div class=" flex flex-row justify-around">
+                        <div class="flex flex-row justify-around">
                             <button type="submit" @click="$emit('close')" :class="btnBase" class="bg-gray-400">いいえ</button>
                             <button type="submit" v-if="isPlusScore && isHome" @click="$emit('plusScore', 'home'), $emit('close')" :class="btnBase" class="bg-blue-500">はい</button>
                             <button type="submit" v-if="isPlusScore && isAway" @click="$emit('plusScore', 'away'), $emit('close')" :class="btnBase" class="bg-blue-500">はい</button>
@@ -39,32 +39,6 @@ const btnBase = 'w-1/4 py-1 text-white rounded-md';
 </template>
 
 <style>
-.modal-mask {
-    position: fixed;
-    z-index: 9998;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.2);
-    display: flex;
-    transition: opacity 0.3s ease;
-}
-
-.modal-container {
-    width: 350px;
-    margin: auto;
-    padding: 20px 30px;
-    background-color: #fff;
-    border-radius: 10px;
-    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
-    transition: all 0.3s ease;
-}
-
-.modal-body {
-    margin: 20px 0;
-}
-
 /*
  * The following styles are auto-applied to elements with
  * transition="modal" when their visibility is toggled
