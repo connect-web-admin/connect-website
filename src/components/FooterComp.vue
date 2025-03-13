b8e19aeb38fdad477c0ebca9c349db0044ce7b5c
OBINO Takuya <espinarosa.obino@gmail.com>
2025-03-13 17:31:24 +0900

    アイコンとリンク修正

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -4,7 +4,7 @@
 <template>
 <div class="flex justify-between items-end py-4 px-6 h-20 border-t-1 text-gray-200">
     <router-link to="/"><img src="@/assets/icons/home.svg" alt="ホーム"></router-link>
-    <router-link to="/championship-schedule"><img src="@/assets/icons/championship-schedule.svg" alt="大会日程"></router-link>
+    <router-link to="/archive"><img src="@/assets/icons/archive.svg" alt="大会日程"></router-link>
     <router-link to="/live-report-for-user"><img src="@/assets/icons/live-report.svg" alt="試合速報"></router-link>
     <router-link to="/contact"><img src="@/assets/icons/contact.svg" alt="コンタクト"></router-link>
     <router-link to="/premium"><img src="@/assets/icons/premium.svg" alt="プレミアム"></router-link>
