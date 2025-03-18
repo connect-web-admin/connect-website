4e993665d90023e7966c15ea3c39bf63fec19129
OBINO Takuya <espinarosa.obino@gmail.com>
2025-03-18 22:51:15 +0900

    SBPS用画像整理

--- src/components/MediaCompForTop.vue
+++ src/components/MediaCompForTop.vue
@@ -53,68 +53,70 @@ onMounted(async () => {
             <p>{{ failedMsg }}</p>
         </div>
         <div v-else>
-            <div v-for="article in latestFourArticles" :key="article.id" class="flex flex-row justify-between items-center gap-2 border-b-1 border-gray-200 py-2">
-                <div class="w-[100px] h-auto">
-                    <img src="../assets/OishiRento_thumbnail.jpg" alt="大石蓮斗" />
+            <div v-for="article in latestFourArticles" :key="article.id" class="flex flex-row h-[100px] justify-between items-center gap-2 border-b-1 border-gray-200 py-2">
+                <div class="w-1/5 h-auto">
+                    <img src="@/assets/top-sample/OishiRento_thumbnail.jpg" alt="大石蓮斗" />
                 </div>
-                <div>
+                <div class="w-4/5 h-full flex flex-col items-start">
                     <div class="flex items-center">
                         <p>{{ article['published_at'] }}</p>
                         <span v-if="isNew(article['published_at'])" class="ml-2">
                             <img src="@/assets/icons/new-notion.svg" alt="new" />
                         </span>
+                        <span class="block bg-red-400 text-white text-xs px-2 py-1 ml-4 rounded-md">クリック可能</span>
                     </div>
                     <div class="flex items-center">
                         <!-- 40文字超過分は省略 -->
-                        <!-- <router-link :to="`/media/${article.article_id}`" class="flex justify-between items-center"> -->
-                            <p class="pl-4">{{ article['title'].length > 40 ? article['title'].substring(0, 40) + '……' : article['title'] }}</p>
-                        <!-- </router-link> -->
+                        <router-link :to="'/media'">
+                            <!-- <router-link :to="`/media/${article.article_id}`"> -->
+                                <p class="pl-4">{{ article['title'].length > 40 ? article['title'].substring(0, 40) + '……' : article['title'] }}</p>
+                        </router-link>
                     </div>
                 </div>
             </div>
-            <div class="flex flex-row justify-between items-center gap-2 border-b-1 border-gray-200 py-2">
-                <div class="w-[100px] h-[100px]">
-                    <img src="../assets/AdobeStock_1.png" alt="サムネ1" class="w-full h-full" />
+            <div class="flex flex-row h-[100px] justify-between items-center gap-2 border-b-1 border-gray-200 py-2">
+                <div class="w-1/5 h-auto">
+                    <img src="@/assets/top-sample/sample1.jpeg" alt="サムネ1" class="w-full h-full" />
                 </div>
-                <div>
+                <div class="w-4/5 h-full flex flex-col items-start">
                     <div class="flex items-center">
                         <p>2025-03-03</p>
                     </div>
                     <div class="flex items-center">
                         <!-- 40文字超過分は省略 -->
-                        <!-- <router-link :to="`/media/${article.article_id}`" class="flex justify-between items-center"> -->
+                        <!-- <router-link :to="`/media/${article.article_id}`"> -->
                             <p class="pl-4">【ダミーデータ】子どもたちがサッカーに触れる機会を確保するべき　若年層育成のリアルな現場の声</p>
                         <!-- </router-link> -->
                     </div>
                 </div>
             </div>
-            <div class="flex flex-row justify-between items-center gap-2 border-b-1 border-gray-200 py-2">
-                <div class="w-[100px] h-auto">
-                    <img src="../assets/AdobeStock_2.png" alt="サムネ2" />
+            <div class="flex flex-row h-[100px] justify-between items-center gap-2 border-b-1 border-gray-200 py-2">
+                <div class="w-1/5 h-auto">
+                    <img src="@/assets/top-sample/sample2.jpeg" alt="サムネ2" />
                 </div>
-                <div>
+                <div class="w-4/5">
                     <div class="flex items-center">
                         <p>2025-03-02</p>
                     </div>
                     <div class="flex items-center">
                         <!-- 40文字超過分は省略 -->
-                        <!-- <router-link :to="`/media/${article.article_id}`" class="flex justify-between items-center"> -->
+                        <!-- <router-link :to="`/media/${article.article_id}`"> -->
                             <p class="pl-4">【ダミーデータ】「元プロサッカー選手にインタビュー」　選手時代も、引退してからも、楽しい！というところは全く変わっていません。</p>
                         <!-- </router-link> -->
                     </div>
                 </div>
             </div>
-            <div class="flex flex-row justify-between items-center gap-2 border-b-1 border-gray-200 py-2">
-                <div class="w-[100px] h-auto">
-                    <img src="../assets/AdobeStock_3.png" alt="サムネ3" />
+            <div class="flex flex-row h-[100px] justify-between items-center gap-2 border-b-1 border-gray-200 py-2">
+                <div class="w-1/5 h-auto">
+                    <img src="@/assets/top-sample/sample3.jpeg" alt="サムネ3" />
                 </div>
-                <div>
+                <div class="w-4/5 h-full flex flex-col items-start">
                     <div class="flex items-center">
                         <p>2025-03-01</p>
                     </div>
                     <div class="flex items-center">
                         <!-- 40文字超過分は省略 -->
-                        <!-- <router-link :to="`/media/${article.article_id}`" class="flex justify-between items-center"> -->
+                        <!-- <router-link :to="`/media/${article.article_id}`"> -->
                             <p class="pl-4">【ダミーデータ】U-15　春の強化合宿に潜入取材！賢いカラダづくりとは？</p>
                         <!-- </router-link> -->
                     </div>
