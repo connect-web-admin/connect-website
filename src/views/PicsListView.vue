483443a1bb284ea7d2e19db3496febf108ae714b
OBINO Takuya <espinarosa.obino@gmail.com>
2025-03-19 11:05:01 +0900

    Initial Commit

---
+++ src/views/PicsListView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+
+</script>
+<template>
+    <div>
+
+    </div>
+</template>
+<style></style>
