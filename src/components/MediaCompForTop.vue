2b530e2ec97e3527fb6f3394e5862ebfbef2917f
OBINO Takuya <espinarosa.obino@gmail.com>
2025-03-19 13:25:38 +0900

    SBPS 画像のパス修正及びサイズ修正

--- src/components/MediaCompForTop.vue
+++ src/components/MediaCompForTop.vue
@@ -55,7 +55,7 @@ onMounted(async () => {
         <div v-else>
             <div v-for="article in latestFourArticles" :key="article.id" class="flex flex-row h-[100px] justify-between items-center gap-2 border-b-1 border-gray-200 py-2">
                 <div class="w-1/5 h-auto">
-                    <img src="@/assets/top-sample/OishiRento_thumbnail.jpg" alt="大石蓮斗" />
+                    <img src="@/assets/top-sample/OishiRento_thumbnail.jpg" alt="大石蓮斗" class="w-full h-full" />
                 </div>
                 <div class="w-4/5 h-full flex flex-col items-start">
                     <div class="flex items-center">
@@ -76,7 +76,7 @@ onMounted(async () => {
             </div>
             <div class="flex flex-row h-[100px] justify-between items-center gap-2 border-b-1 border-gray-200 py-2">
                 <div class="w-1/5 h-auto">
-                    <img src="@/assets/top-sample/sample1.jpeg" alt="サムネ1" class="w-full h-full" />
+                    <img src="../assets/top-sample/sample1.jpeg" alt="サムネ1" class="w-full h-full" />
                 </div>
                 <div class="w-4/5 h-full flex flex-col items-start">
                     <div class="flex items-center">
@@ -92,7 +92,7 @@ onMounted(async () => {
             </div>
             <div class="flex flex-row h-[100px] justify-between items-center gap-2 border-b-1 border-gray-200 py-2">
                 <div class="w-1/5 h-auto">
-                    <img src="@/assets/top-sample/sample2.jpeg" alt="サムネ2" />
+                    <img src="../assets/top-sample/sample2.jpeg" alt="サムネ2" />
                 </div>
                 <div class="w-4/5">
                     <div class="flex items-center">
@@ -108,7 +108,7 @@ onMounted(async () => {
             </div>
             <div class="flex flex-row h-[100px] justify-between items-center gap-2 border-b-1 border-gray-200 py-2">
                 <div class="w-1/5 h-auto">
-                    <img src="@/assets/top-sample/sample3.jpeg" alt="サムネ3" />
+                    <img src="../assets/top-sample/sample3.jpeg" alt="サムネ3" />
                 </div>
                 <div class="w-4/5 h-full flex flex-col items-start">
                     <div class="flex items-center">
