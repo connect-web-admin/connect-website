152adeaff56c686586e28af6021fe5903c910aeb
OBINO Takuya <espinarosa.obino@gmail.com>
2025-03-19 11:03:20 +0900

    個別でクラブ情報を表示させるページに変更

--- src/views/ClubIntroductionView.vue
+++ src/views/ClubIntroductionView.vue
@@ -1,10 +1,16 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-import { CLUB_API_URL, ID_TOKEN_FOR_AUTH } from '@/utils/constants'
+import { ref, onMounted } from 'vue';
+import { useRouter } from 'vue-router';
 
-const isLoading = ref(false)
-const idTokenForAuth = localStorage.getItem(ID_TOKEN_FOR_AUTH)
-const clubInfo = ref({})
+const router = useRouter();
+
+// import { CLUB_API_URL, ID_TOKEN_FOR_AUTH } from '@/utils/constants'
+
+
+
+// const isLoading = ref(false)
+// const idTokenForAuth = localStorage.getItem(ID_TOKEN_FOR_AUTH)
+// const clubInfo = ref({})
 
 // const getAllClubInfo = async () => {
 //     isLoading.value = true
@@ -30,27 +36,33 @@ const clubInfo = ref({})
 //         isLoading.value = false
 //     }
 // }
-const ichiran = ref(31)
-// onMounted(async () => {
-//     await getAllClubInfo()
-// })
 </script>
 
 <template>
-<div class="main-wrapper">
-    <div v-for="(item, index) in ichiran">
-        <p>{{ index }}クラブ一覧</p>
-    </div>
-    <!-- <div v-if="isLoading">
-        読み込み中
-    </div>
-    <div v-else class="team-logos-wrapper">
-        <p>ENTRY TEAMS</p>
-        <div v-for="item in clubInfo" class="each-logo-container">
-            {{ item['club_images']['introduction_image']['image_url'] }}
-            <p>{{ item['club_name'] }}</p>
+<div>
+    <div class="max-w-4xl mx-auto p-6">
+        <h1 class="text-3xl font-bold text-center mb-6">札幌市中央区ジュニアチームA</h1>
+        <img src="@/assets/top-sample/football-club-players-photo.jpeg" alt="札幌市中央区ジュニアチームA" class="w-full h-auto">
+        <div class="space-y-2">
+            <p><strong>HP:</strong> <a href="#" class="text-blue-500 underline">https://sapporoshichuoukujuniorteama.com</a></p>
+            <p><strong>所在地:</strong> 札幌市中央区南S条西T丁目U-V</p>
+            <p><strong>電話番号:</strong> 011-XXXX-XXXX</p>
+            <p><strong>実績:</strong></p>
+            <ul class="list-disc list-inside ml-4">
+                <li>A大会 優勝</li>
+                <li>B大会 ベスト8</li>
+            </ul>
+            <p><strong>監督名:</strong> 田中 太郎</p>
+            <p><strong>コーチ名:</strong> 佐藤 花子</p>
+            <p><strong>在籍者数:</strong> 25名</p>
+            <p><strong>所有カテゴリ:</strong> 第２種,第３種,第４種, WOMAN</p>
+            <p><strong>セールスポイント:</strong></p>
+            <p class="border-l-4 border-blue-500 pl-4">
+                札幌市中央区ジュニアチームAは、札幌市中央区を拠点に活動するチームです。技術と戦術の向上を重視し、
+                幅広い世代のプレイヤーが活躍できる環境を提供しています。
+            </p>
         </div>
-    </div> -->
+    </div>
 </div>
 </template>
 
