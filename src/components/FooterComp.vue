ee64ab6e4d809c68eabe7ba1c536a5dc41012345
OBINO Takuya <espinarosa.obino@gmail.com>
2025-03-28 16:53:02 +0900

    フッターを小さくした（アイコン等サイズ調整）

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,13 +1,14 @@
 <script setup>
+const imgSize = 'w-[40px] h-auto';
 </script>
 
 <template>
-<div class="flex justify-between items-end py-4 px-6 h-20 border-t-1 bg-white">
-    <router-link to="/"><img src="@/assets/icons/home.svg" alt="ホーム"></router-link>
-    <router-link to="/archive"><img src="@/assets/icons/archive.svg" alt="大会日程"></router-link>
-    <router-link to="/live-report-for-user"><img src="@/assets/icons/live-report.svg" alt="試合速報"></router-link>
-    <router-link to="/contact"><img src="@/assets/icons/contact.svg" alt="コンタクト"></router-link>
-    <router-link to="/premium"><img src="@/assets/icons/premium.svg" alt="プレミアム"></router-link>
+<div class="flex flex-row justify-center gap-x-8 pb-2 items-end border-t-1 bg-white">
+    <router-link to="/"><img src="@/assets/icons/home.svg" alt="ホーム" :class="imgSize"></router-link>
+    <router-link to="/archive"><img src="@/assets/icons/archive.svg" alt="大会日程" :class="imgSize"></router-link>
+    <router-link to="/live-report-for-user"><img src="@/assets/icons/live-report.svg" alt="試合速報" :class="imgSize"></router-link>
+    <router-link to="/contact"><img src="@/assets/icons/contact.svg" alt="コンタクト" :class="imgSize"></router-link>
+    <router-link to="/premium"><img src="@/assets/icons/premium.svg" alt="プレミアム" :class="imgSize"></router-link>
 </div>
 </template>
 
