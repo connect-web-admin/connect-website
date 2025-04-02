8a6feb583d6e9c8f1c1b2d45de8f9c44c626a095
OBINO Takuya <espinarosa.obino@gmail.com>
2025-04-02 18:39:37 +0900

    リンク修正

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -4,7 +4,7 @@ const imgSize = 'w-[40px] h-auto';
 
 <template>
 <div class="flex flex-row justify-center gap-x-8 pb-2 items-end border-t-1 bg-white">
-    <router-link to="/"><img src="@/assets/icons/home.svg" alt="ホーム" :class="imgSize"></router-link>
+    <router-link to="/top"><img src="@/assets/icons/home.svg" alt="ホーム" :class="imgSize"></router-link>
     <router-link to="/archive"><img src="@/assets/icons/archive.svg" alt="大会日程" :class="imgSize"></router-link>
     <router-link to="/live-report-for-user"><img src="@/assets/icons/live-report.svg" alt="試合速報" :class="imgSize"></router-link>
     <router-link to="/contact"><img src="@/assets/icons/contact.svg" alt="コンタクト" :class="imgSize"></router-link>
