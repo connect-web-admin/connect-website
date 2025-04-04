00c29401f8ad426a6a08de7eca0cab10d726a7e1
OBINO Takuya <espinarosa.obino@gmail.com>
2025-04-04 20:09:32 +0900

    新規登録と認証コード確認画面は認証が不要

--- src/App.vue
+++ src/App.vue
@@ -54,11 +54,12 @@ const isConnecterPath = () => {
 }
 
 /**
- * connecterのパスかどうかを判定する関数
- * connecterのパスは、試合を速報する時にしか使わず、ヘッダーとフッターは邪魔になるため表示しないようにする
+ * 認証が不要なパスかどうかを判定する関数
+ * 新規登録と認証コード確認画面は認証が不要
  */
 const isSignUpPath = () => {
-    return router.currentRoute.value.path.startsWith('/signup');
+    const path = router.currentRoute.value.path;
+    return path.startsWith('/signup') || path.startsWith('/confirm-signup');
 }
 
 
@@ -92,8 +93,12 @@ onMounted(() => {
     </header>          
 
     <main class="flex-1 pb-20">
-        <template v-if="!isSignUpPath()">
-            <!-- <Authenticator :hideSignUp="true" :login-mechanisms="['email']">
+        <RouterView />
+        <!-- <template v-if="isSignUpPath()">
+            <RouterView />
+        </template>
+        <template v-else>
+            <Authenticator :hideSignUp="true" :login-mechanisms="['email']">
                 <template v-slot="{ user }">
                     <RouterView />
                 </template>
@@ -108,11 +113,7 @@ onMounted(() => {
                 <router-link to="/signup" class="text-center bg-lime-600 text-white px-4 py-2 rounded-sm">
                     新規会員登録
                 </router-link>
-            </div> -->
-            <RouterView />
-        </template>
-        <!-- <template v-else>
-            <RouterView />
+            </div>
         </template> -->
     </main>
         
