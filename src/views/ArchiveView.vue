3549be20fb17957feda3b4bcc80563be21378098
OBINO Takuya <espinarosa.obino@gmail.com>
2025-04-09 16:20:54 +0900

    大会日程ページテンプレ

--- src/views/ArchiveView.vue
+++ src/views/ArchiveView.vue
@@ -1,27 +1,56 @@
 <script setup>
 import { ref } from 'vue';
 
-const isLoading = ref(false);
-const failedMsg = ref('');
-
+// const isLoading = ref(false);
+// const failedMsg = ref('');
+// CSS
+const eachChampionship = 'p-3 border-b border-gray-200'
+const categorySection = 'mt-2 p-2 border border-gray-300 rounded-lg'
+const listItemStyle = 'py-2 border-b border-gray-200 last:border-b-0'
 </script>
 
 <template>
     <div class="p-5">
-        <div v-if="isLoading" class="mt-20">
-            <img src="../assets/icons/loading.gif" alt="読み込み中" class="w-10 h-10 mx-auto">
+        <div>
+            <h1 class="text-xl font-bold">高円宮杯 JFA U-18サッカーリーグ2025北海道 ブロックリーグ札幌</h1>
+            <!-- <img src="" alt="高円宮杯 JFA U-18サッカーリーグ2025北海道 ブロックリーグ札幌 星取表" /> -->
         </div>
-        <div v-else-if="failedMsg">
-            <p>{{ failedMsg }}</p>
-        </div>
-        <div v-else>
-            <h1 class="text- font-bold mb-5">U-18プリンスリーグの大会詳細結果について</h1>
-            <div>
-                <p>U-18プリンスリーグの大会詳細結果についてはこちらで見ることができます。</p>
-                <a href="https://www.jfa.jp/match/takamado_jfa_u18_prince2025/hokkaido/" target="_blank"
-                    class="text-blue-500 underline break-words">
-                    https://www.jfa.jp/match/takamado_jfa_u18_prince2025/hokkaido/
-                </a>
+        <div class="mt-15">
+            <h1 class="text-xl font-bold">アーカイブ</h1>
+            <h2>2024年度（タップで画像を表示します）</h2>
+            <div :class="categorySection" >
+                <h3 class="text-xl font-semibold">２種</h3>
+                <ul>
+                    <li :class="listItemStyle">高円宮杯 JFA U-18サッカー2024北海道 ブロックリーグ札幌</li>
+                    <li :class="listItemStyle">【大会名入力】</li>
+                    <li :class="listItemStyle">【大会名入力】</li>
+                    <li :class="listItemStyle">【大会名入力】</li>
+                    <li :class="listItemStyle">【大会名入力】</li>
+                </ul>
+            </div>
+            <div :class="categorySection">
+                <h3 class="text-xl font-semibold mb-2">３種</h3>
+                <ul>
+                    <li :class="listItemStyle">【大会名入力】</li>
+                    <li :class="listItemStyle">【大会名入力】</li>
+                    <li :class="listItemStyle">【大会名入力】</li>
+                    <li :class="listItemStyle">【大会名入力】</li>
+                    <li :class="listItemStyle">【大会名入力】</li>
+                    <li :class="listItemStyle">【大会名入力】</li>
+                </ul>
+            </div>
+            <div :class="categorySection">
+                <h3 class="text-xl font-semibold mb-2">４種</h3>
+                <ul>
+                    <li :class="listItemStyle">【大会名入力】</li>
+                    <li :class="listItemStyle">【大会名入力】</li>
+                    <li :class="listItemStyle">【大会名入力】</li>
+                    <li :class="listItemStyle">【大会名入力】</li>
+                    <li :class="listItemStyle">【大会名入力】</li>
+                    <li :class="listItemStyle">【大会名入力】</li>
+                    <li :class="listItemStyle">【大会名入力】</li>
+                    <li :class="listItemStyle">【大会名入力】</li>
+                </ul>
             </div>
         </div>
     </div>
