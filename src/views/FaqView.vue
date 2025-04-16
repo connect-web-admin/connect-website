3853c61f47ce24fdcdfccec324b592094b7e6095
OBINO Takuya <espinarosa.obino@gmail.com>
2025-04-16 15:40:33 +0900

    FAQページ作成

--- src/views/FaqView.vue
+++ src/views/FaqView.vue
@@ -1,5 +1,29 @@
 <script setup>
 </script>
 <template>
+    <div class="p-4 space-y-10">
+        <div class='space-y-4 border border-gray-200 rounded-lg p-4'>
+            <div class="flex items-center gap-2 border-b border-gray-200 pb-4">
+                <img src="../assets/icons/question-mark.svg" alt="FAQ" class="w-[30px] h-auto" />
+                <p>試合の写真をダウンロードしたい。</p>
+            </div>
+            <div class="flex items-start gap-2">
+                <img src="../assets/icons/exclamation-mark.svg" alt="FAQ" class="pl-[2px] w-[29px] h-auto" />
+                <p>写真を選んで「保存する」ボタンを押していただくとダウンロードしていただけます（お使いの機種により操作が異なる場合がございます。お使いのブラウザの使用方法をご確認ください）。</p>
+            </div>
+        </div>
+
+        <div class='space-y-4 border border-gray-200 rounded-lg p-4'>
+            <div class="flex items-center gap-2 border-b border-gray-200 pb-4">
+                <img src="../assets/icons/question-mark.svg" alt="FAQ" class="w-[30px] h-auto" />
+                <p>退会したい。</p>
+            </div>
+            <div class="flex items-start gap-2">
+                <img src="../assets/icons/exclamation-mark.svg" alt="FAQ" class="pl-[2px] w-[29px] h-auto" />
+                <p>退会につきましては、退会を希望される月の前の月の20日までに、会員情報ページに記載の会員IDを添えて<a href="mailto:info@connect-goals.com" class="text-blue-500 underline">Connectまでご連絡</a>ください。<br />
+                    （例えば、６月15日にご連絡いただきますと、７月初日以降はご利用いただけなくなります。）</p>
+            </div>
+        </div>
+    </div>
 </template>
 <style></style>
