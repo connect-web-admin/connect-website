7587260f3b615e32fcf5cbc5f98623ffad277e18
OBINO Takuya <espinarosa.obino@gmail.com>
2025-04-16 09:20:41 +0900

    checkUserBeforeLoginLambda完成

--- amplify/backend/function/checkUserBeforeLoginLambda/src/index.js
+++ amplify/backend/function/checkUserBeforeLoginLambda/src/index.js
@@ -1,17 +1,51 @@
+const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+const { DynamoDBDocumentClient, GetCommand, QueryCommand, ScanCommand } = require('@aws-sdk/lib-dynamodb');
 
+const ddbClient = new DynamoDBClient({ region: "ap-northeast-1" });
+const ddbDocClient = DynamoDBDocumentClient.from(ddbClient);
 
-/**
- * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
- */
+// メインハンドラー
 exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  },
-        body: JSON.stringify('Hello from Lambda!'),
-    };
+    console.log("認証前トリガー event:", JSON.stringify(event, null, 2));
+
+    // ログイン情報（メールアドレス）を取得
+    const inputEmail = event.request.userAttributes.email;
+
+    // メールアドレスが空の場合はエラーを投げる
+    if (!inputEmail) {
+        throw new Error("メールアドレスが空です");
+    }
+
+    // メールアドレスをキーにしてユーザー情報を取得
+    // can_loginがfalseの場合はログインを拒否
+    try {
+        const queryParams = {
+            TableName: 'MemberDDB-dev',
+            IndexName: 'gsiByEmail',
+            KeyConditionExpression: 'email = :email',
+            ExpressionAttributeValues: {
+                ':email': inputEmail
+            }
+        };
+
+        console.log('登録試行パラメータ', queryParams);
+
+        const command = new QueryCommand(queryParams);
+        const result = await ddbDocClient.send(command);
+
+        console.log("DynamoDBから取得したデータ:", result);
+
+        // can_loginがfalseの場合はログインを拒否
+        if (!result.Items || result.Items[0].can_login === false) {
+            throw new Error("ログインできません");
+        }
+
+        // 問題なければログインを許可
+        return event;
+    } catch (err) {
+        console.error("ログイン前チェックでエラー:", err);
+
+        // エラーを投げると Cognito はログインをブロックする
+        throw new Error("ログインが拒否されました: " + err.message);
+    }
 };
