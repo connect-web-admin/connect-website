4376fbde68a7f293ff84a5f878d58c7994c534e0
OBINO Takuya <espinarosa.obino@gmail.com>
2025-04-22 19:36:04 +0900

    大会情報を取得してループ表示

--- src/views/PicsView.vue
+++ src/views/PicsView.vue
@@ -1,13 +1,23 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import { useRouter } from "vue-router";
-import { MATCH_API_URL, FEP_API_URL } from "@/utils/constants";
+import { MATCH_API_URL, THIS_FISCAL_YEAR } from "@/utils/constants";
 
 const router = useRouter();
 const championshipInfo = ref([]);
+const noChampionshipMsg = ref('');
+const isLoading = ref(true);
+
+// カテゴリーの表示順序を定義
+const categoryOrder = [
+    'U-12（ジュニア）',
+    'U-15（ジュニアユース）',
+    'U-18（ユース）',
+    'WOMAN'
+];
 
 const getChampionshipInfo = async () => {
-    const queryUrl = new URL(`${MATCH_API_URL}/matches-in-this-week`);
+    const queryUrl = new URL(`${MATCH_API_URL}/championship-names-ids`);
     queryUrl.searchParams.append('fiscalYear', THIS_FISCAL_YEAR);
 
     try {
@@ -22,19 +32,38 @@ const getChampionshipInfo = async () => {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
 
-        matchInfo.value = await response.json();
-        if (matchInfo.value.length > 0) {
+        const data = await response.json();
+        
+        // カテゴリーごとにデータをグループ化
+        const groupedData = data.reduce((acc, item) => {
+            if (!acc[item.category]) {
+                acc[item.category] = [];
+            }
+            acc[item.category].push(item);
+            return acc;
+        }, {});
+
+        // カテゴリーの順序に従ってソート
+        const sortedData = {};
+        categoryOrder.forEach(category => {
+            // データが存在しない場合でも空の配列を設定
+            sortedData[category] = groupedData[category] || [];
+        });
+
+        championshipInfo.value = sortedData;
+
+        if (Object.keys(championshipInfo.value).length > 0) {
             return true;
         } else {
-            noThisWeekMatchesMsg.value = '今週開催予定の試合はありません。';
+            noChampionshipMsg.value = '大会情報がありません。';
             return false;
         }
     } catch (error) {
         console.error('速報対象試合の取得に失敗しました。');
+    } finally {
+        isLoading.value = false;
     }
 }
-
-
 
 onMounted(async () => {
     // ページ遷移時に最上部へスクロール
@@ -45,45 +74,30 @@ onMounted(async () => {
 
     // 大会情報を取得
     await getChampionshipInfo();
-
-    // 大会情報を取得
-    // const championshipId = router.currentRoute.value.params.championshipId;
-    // console.log(championshipId);
 });
 </script>
 <template>
     <div>
-        <div>
-            <h2 class="font-bold bg-gray-200 p-2">U-18（ユース）</h2>
-            <ul>
-                <li class="p-2">
-                    <router-link to="/pics-list" class="block w-full"
-                        >高円宮杯 JFA U-18 サッカー2025北海道
-                        ブロックリーグ札幌</router-link
-                    >
-                </li>
-            </ul>
+        <div v-if="isLoading" class="mt-20">
+            <img src="../assets/icons/loading.gif" alt="読み込み中" class="w-10 h-10 mx-auto">
         </div>
-
-        <div>
-            <h2 class="font-bold bg-gray-200 p-2">U-15（ジュニアユース）</h2>
-            <ul>
-                <li class="p-2">写真が登録されていません。</li>
-            </ul>
-        </div>
-
-        <div>
-            <h2 class="font-bold bg-gray-200 p-2">U-12（ジュニア）</h2>
-            <ul>
-                <li class="p-2">写真が登録されていません。</li>
-            </ul>
-        </div>
-
-        <div>
-            <h2 class="font-bold bg-gray-200 p-2">WOMAN</h2>
-            <ul>
-                <li class="p-2">写真が登録されていません。</li>
-            </ul>
+        <div v-else>
+            <div v-for="(championships, category) in championshipInfo" :key="category">
+                <h2 class="font-bold bg-gray-200 p-2 mb-2">{{ category }}</h2>
+                <ul class="pb-2 pr-4 list-disc list-outside ml-8">
+                    <li class="pb-2" v-for="championship in championships" :key="championship.championship_id">
+                        <span v-if="championship.championship_id === 'TakamadonomiyaCup2025HokkaidoSapporoBlock'">
+                            <router-link :to="`/pics-list/${championship.championship_id}`" class="w-full text-blue-600 flex justify-between items-center">
+                                <p class="pr-2">{{ championship.championship_name }}</p>
+                                <img src="../assets/icons/arrow-right.png" alt="右矢印" class="w-4 h-4 inline-block">
+                            </router-link>
+                        </span>
+                        <span v-else>
+                            {{ championship.championship_name }}
+                        </span>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </template>
