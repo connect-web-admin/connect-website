89949ea131c7438c3ab8985c5d41f5dac7d9b288
OBINO Takuya <espinarosa.obino@gmail.com>
2025-04-23 11:39:21 +0900

    PicsList暫定完成（縦３列、モーダル表示）

--- src/views/PicsListView.vue
+++ src/views/PicsListView.vue
@@ -1,115 +1,3 @@
-<!-- <script setup>
-import { ref, onMounted } from 'vue';
-import { useRouter } from 'vue-router';
-
-const router = useRouter();
-const selectedImage = ref(null);
-const showModal = ref(false);
-
-const images = [
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7881.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7884.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7886.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7892.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7893.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7895.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7898.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7903.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7904.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7909.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7913.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7916.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7920.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7922.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7924.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7925.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7926.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7927.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7930.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7933.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7934.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7935.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7936.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7937.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7939.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7944.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7945.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7947.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7954.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7955.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7957.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7961.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7970.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7975.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7980.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7984.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7988.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7990.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7992.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7993.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7994.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7997.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_7998.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_8000.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_8005.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_8006.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_8008.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_8011.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_8012.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_8016.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_8018.JPG' },
-    { src: 'https://connect-website-bucket0c0f1-dev.s3.amazonaws.com/pic/TakamadonomiyaCup2025HokkaidoSapporoBlock/04-12/IMG_8023.JPG' }
-];
-
-const openModal = (index) => {
-    selectedImage.value = index;
-    showModal.value = true;
-};
-
-const closeModal = () => {
-    showModal.value = false;
-    selectedImage.value = null;
-};
-
-// const getPics = async () => {
-//     const data = await response.json();
-// };
-
-// onMounted(async () => {
-//     await getPics();
-// });
-
-onMounted(() => {
-  // ページ遷移時に最上部へスクロール
-    window.scrollTo({
-        top: 0,
-        behavior: 'auto'
-    });
-});
-</script> -->
-
-<!-- <template>
-    <div class="p-4">
-        <h1 class="text-2xl font-bold py-2">高円宮杯 JFA U-18 サッカー2025北海道 ブロックリーグ札幌</h1>
-        <div class="grid grid-cols-3 gap-4">
-            <div v-for="(image, index) in images" :key="index" class="relative cursor-pointer aspect-square"
-                @click="openModal(index)">
-                <img :src="image.src" alt="高円宮杯 JFA U-18 サッカー2025北海道 ブロックリーグ札幌" class="w-full h-full object-cover">
-            </div>
-        </div>
-        <div v-if="showModal" class="fixed inset-0 bg-black/70 flex items-center justify-center z-50 p-4"
-            @click="closeModal">
-            <div class="relative w-[98vw] flex items-center justify-center">
-                <img :src="images[selectedImage].src" alt="札幌未来杯" class="w-full h-auto object-contain">
-                <button @click.stop="closeModal"
-                    class="absolute top-4 right-4 text-white bg-black/50 rounded-full w-8 h-8 flex items-center justify-center">
-                    ✕
-                </button>
-            </div>
-        </div>
-    </div>
-</template> -->
-
 <script setup>
 import { ref, onMounted, computed } from "vue";
 import { useRouter } from "vue-router";
@@ -128,12 +16,21 @@ const isLoading = ref(true);
 const targetPics = ref([]);
 const matchDates = ref([]);
 const failedMsg = ref("");
+const noTargetPicsMsg = ref("");
+
+const openModal = (index) => {
+    selectedImage.value = index;
+    showModal.value = true;
+};
+
+const closeModal = () => {
+    showModal.value = false;
+    selectedImage.value = null;
+};
 
 /**
  * "M/D" または "MM/DD" 形式の文字列を
- * "YYYY-MM-DD" 形式に変換する
- * @param {string} mAndD 例: "4/3", "04/13"
- * @returns {string} 例: "2025-04-03"
+ * "YYYY-MM-DD" 形式に変換
  */
 function toIsoDate(mAndD) {
     // スラッシュで分割して数値化
@@ -196,6 +93,13 @@ const getTargetPics = async (matchDate) => {
         }
 
         targetPics.value = await response.json();
+
+        if (targetPics.value.length === 0) {
+            noTargetPicsMsg.value = "該当する画像はありません。";
+        }
+        if (targetPics.value.length > 0) {
+            noTargetPicsMsg.value = "";
+        }
     } catch (error) {
         failedMsg.value =
             "画像の取得に失敗しました。ブラウザを更新するか、時間を置いてからアクセスしてください。それでも改善されない場合は、Connectまでお問い合わせください。";
@@ -224,7 +128,7 @@ onMounted(async () => {
     <div>
         <div class="p-2 text-sm">
             <h1 class="text-lg font-bold py-2">{{ championshipName }}</h1>
-            試合日を選択してください。<br />
+            <p>試合日を選択してください。</p>
             <span
                 v-for="matchDate in matchDates"
                 :key="matchDate.article_id"
@@ -236,6 +140,32 @@ onMounted(async () => {
                     >{{ matchDate }}</span
                 >
             </span>
+        </div>
+        <div>
+            <div v-if="isLoading" class="mt-20">
+                <img src="../assets/icons/loading.gif" alt="読み込み中" class="w-10 h-10 mx-auto">
+            </div>
+            <div v-else>
+                <div v-if="noTargetPicsMsg" class="mt-20">
+                    <p class="text-center text-gray-500">{{ noTargetPicsMsg }}</p>
+                </div>
+                <div v-else class="grid grid-cols-3 gap-3 px-2">
+                    <div v-for="(pic, index) in targetPics" :key="index" class="relative cursor-pointer aspect-square"
+                        @click="openModal(index)">
+                        <img :src="pic.pic_url" alt="高円宮杯 JFA U-18 サッカー2025北海道 ブロックリーグ札幌" class="w-full h-full object-cover">
+                    </div>
+                </div>
+                <div v-if="showModal" class="fixed inset-0 bg-black/70 flex items-center justify-center z-50 p-4"
+                    @click="closeModal">
+                    <div class="relative w-[98vw] flex items-center justify-center">
+                        <img :src="targetPics[selectedImage].pic_url" alt="高円宮杯 JFA U-18 サッカー2025北海道 ブロックリーグ札幌" class="w-full h-auto object-contain">
+                        <button @click.stop="closeModal"
+                            class="absolute top-2 right-2 text-white bg-black/50 rounded-full w-5 h-5 flex items-center justify-center">
+                            ✕
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
