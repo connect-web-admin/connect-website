5e01e4229bcd944e709ea5c823c618fa42eb6186
OBINO Takuya <espinarosa.obino@gmail.com>
2025-06-03 13:18:30 +0900

    FAQのログアウトボタンをタップした時の挙動を微調整

--- src/views/FaqView.vue
+++ src/views/FaqView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { USER_ATTR_SESSION_ID, USER_ATTR_EMAIL, MEMBER_API_URL } from '@/utils/constants';
 
@@ -15,6 +15,8 @@ const props = defineProps({
         default: () => { }
     },
 });
+
+const isLoading = ref(false);
 
 /**
  * session_idを削除する関数
@@ -50,6 +52,7 @@ const removeSessionIdInMemberDDB = async () => {
  * ログアウトしてローカルストレージのアイテムを削除
  */
 const signOutAndDeleteItemsInLocalStorage = async () => {
+    isLoading.value = true;
     await removeSessionIdInMemberDDB();
 
     // ローカルストレージのアイテムを削除
@@ -64,6 +67,8 @@ const signOutAndDeleteItemsInLocalStorage = async () => {
     // Authenticator備え付けのログアウト用の関数
     props.signOut();
 
+    isLoading.value = false;
+
     router.push('/');
 }
 
@@ -76,42 +81,48 @@ onMounted(() => {
 });
 </script>
 <template>
-    <div class="p-4 space-y-10">
-        <div class='space-y-4 border border-gray-200 rounded-lg p-4'>
-            <div class="flex items-center gap-2 border-b border-gray-200 pb-4">
-                <img src="../assets/icons/question-mark.svg" alt="FAQ" class="w-[30px] h-auto" />
-                <p>試合の写真をダウンロードしたい。</p>
-            </div>
-            <div class="flex items-start gap-2">
-                <img src="../assets/icons/exclamation-mark.svg" alt="FAQ" class="pl-[2px] w-[29px] h-auto" />
-                <p>写真を選んで「保存する」ボタンを押していただくとダウンロードしていただけます（お使いの機種により操作が異なる場合がございます。お使いのブラウザの使用方法をご確認ください）。</p>
-            </div>
+    <div>
+        <div v-if="isLoading" class="mt-20">
+            <img src="../assets/icons/loading.gif" alt="読み込み中" class="w-10 h-10 mx-auto">
         </div>
+        <div v-else class="p-4 space-y-10">
+            <div class='space-y-4 border border-gray-200 rounded-lg p-4'>
+                <div class="flex items-center gap-2 border-b border-gray-200 pb-4">
+                    <img src="../assets/icons/question-mark.svg" alt="FAQ" class="w-[30px] h-auto" />
+                    <p>試合の写真をダウンロードしたい。</p>
+                </div>
+                <div class="flex items-start gap-2">
+                    <img src="../assets/icons/exclamation-mark.svg" alt="FAQ" class="pl-[2px] w-[29px] h-auto" />
+                    <p>写真を選んで「保存する」ボタンを押していただくとダウンロードしていただけます（お使いの機種により操作が異なる場合がございます。お使いのブラウザの使用方法をご確認ください）。</p>
+                </div>
+            </div>
 
-        <div class='space-y-4 border border-gray-200 rounded-lg p-4'>
-            <div class="flex items-center gap-2 border-b border-gray-200 pb-4">
-                <img src="../assets/icons/question-mark.svg" alt="FAQ" class="w-[30px] h-auto" />
-                <p>ログアウトしたい。</p>
+            <div class='space-y-4 border border-gray-200 rounded-lg p-4'>
+                <div class="flex items-center gap-2 border-b border-gray-200 pb-4">
+                    <img src="../assets/icons/question-mark.svg" alt="FAQ" class="w-[30px] h-auto" />
+                    <p>ログアウトしたい。</p>
+                </div>
+                <div class="flex items-start gap-2">
+                    <img src="../assets/icons/exclamation-mark.svg" alt="FAQ" class="pl-[2px] w-[29px] h-auto" />
+                    <router-link @click="signOutAndDeleteItemsInLocalStorage" to="/top" class="text-blue-500 underline">
+                        こちらをタップ（クリック）してください。
+                    </router-link>
+                </div>
             </div>
-            <div class="flex items-start gap-2">
-                <img src="../assets/icons/exclamation-mark.svg" alt="FAQ" class="pl-[2px] w-[29px] h-auto" />
-                <router-link @click="signOutAndDeleteItemsInLocalStorage" to="/top" class="text-blue-500 underline">
-                    こちらをタップ（クリック）してください。
-                </router-link>
-            </div>
-        </div>
 
-        <div class='space-y-4 border border-gray-200 rounded-lg p-4'>
-            <div class="flex items-center gap-2 border-b border-gray-200 pb-4">
-                <img src="../assets/icons/question-mark.svg" alt="FAQ" class="w-[30px] h-auto" />
-                <p>退会したい。</p>
-            </div>
-            <div class="flex items-start gap-2">
-                <img src="../assets/icons/exclamation-mark.svg" alt="FAQ" class="pl-[2px] w-[29px] h-auto" />
-                <p>退会につきましては、退会を希望される月の前の月の20日までに、会員情報ページに記載の会員IDとお名前を添えて<a href="mailto:info@connect-goals.com" class="text-blue-500 underline">Connectまでご連絡</a>ください。<br />
-                    （例えば、６月15日にご連絡いただきますと、７月初日以降はご利用いただけなくなります。）</p>
+            <div class='space-y-4 border border-gray-200 rounded-lg p-4'>
+                <div class="flex items-center gap-2 border-b border-gray-200 pb-4">
+                    <img src="../assets/icons/question-mark.svg" alt="FAQ" class="w-[30px] h-auto" />
+                    <p>退会したい。</p>
+                </div>
+                <div class="flex items-start gap-2">
+                    <img src="../assets/icons/exclamation-mark.svg" alt="FAQ" class="pl-[2px] w-[29px] h-auto" />
+                    <p>退会につきましては、退会を希望される月の前の月の20日までに、会員情報ページに記載の会員IDとお名前を添えて<a href="mailto:info@connect-goals.com" class="text-blue-500 underline">Connectまでご連絡</a>ください。<br />
+                        （例えば、６月15日にご連絡いただきますと、７月初日以降はご利用いただけなくなります。）</p>
+                </div>
             </div>
         </div>
     </div>
+
 </template>
 <style></style>
