99ce16e1f91c9b03b7837ff6a2e23863b50fd54d
OBINO Takuya <espinarosa.obino@gmail.com>
2025-06-18 15:24:46 +0900

    ライトプラン会員がレギュラー会員だけが見られるコンテンツを見ようとした時に表示されるページ作成

---
+++ src/views/UnauthenticatedView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  // ページ遷移時に最上部へスクロール
+    window.scrollTo({
+        top: 0,
+        behavior: 'auto'
+    });
+});
+</script>
+<template>
+    <div class="p-4 h-100 flex items-center justify-center">
+        <h1 class="text-2xl font-bold absolute top-4">アクセス権限エラー</h1>
+        <div class="text-xl">
+            本コンテンツはレギュラー会員専用です。
+        </div>
+    </div>
+</template>
+<style></style>
